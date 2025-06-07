1b1539e77aca08ca6921802d90790993c9f8f9b1
Tomaz <tomaz.plavcak505@gmail.com>
2025-06-07 12:46:48 +0200

    funkcije.jl

--- funkcije.jl
+++ funkcije.jl
@@ -1,4 +1,4 @@
-function Lagrange(xi::Vector{Number},yi::Vectot{Number})
+function Lagrange(xi::Vector{<:Number},yi::Vector{<:Number})
 
 	n = length(xi)
 	
@@ -6,10 +6,34 @@ function Lagrange(xi::Vector{Number},yi::Vectot{Number})
 		error("yi prekratek")
 	end
 
+	ci = [ yi[i]/prod(xi[setdiff(1:n,i)].-xi[i]) for i=1:n]
 
-	f(x::Number) = sum([ yi[i]/prod(xi[setdiff(1:n,i)] .- xi[i] )*prod(xi[setdiff(1:n,i)].-x) for i = 1:n])
+
+	f(x::Number) = sum([ ci[i] * prod(xi[setdiff(1:n,i)].-x) for i = 1:n])
 	
-	f(x::Vector{Number}) = [ f(x::Number) for x =x ]
+	f(x::Vector{<:Number}) = [ f(x::Number) for x =x ]
 
 	return f
 end
+
+
+
+"""LeastSquares(xi::vector{<:Number},yi::Vector{<:Number};n = 1)
+
+	f = LeastSquares(xi,yi) = sum( ai * x^i )
+
+	f(x::Number) & f(x::Vector{<:Number})
+"""
+function LeastSquares(xi::Vector{<:Number},yi::Vector{<:Number};n = 1)
+	A = hcat([[ sum(xi.^(i+j)) for i=0:n ] for j=0:n] ...)
+	b = [ sum(yi.*(xi.^i)) for i =0:n]
+	a = A\b
+
+
+	f(x::Number) = a' * x.^(0:n)
+	f(x::Vector{<:Number}) = [f(x::Number) for x=x]
+
+	return f
+end
+
+
